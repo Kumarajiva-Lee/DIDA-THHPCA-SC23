36e85d5323f28430b52317e04edcbd1d410d15f7
jql2019011556 <jiaqilong2000@163.com>
2023-06-21 23:24:00 +0800

    fix coupler and python process

--- code/x86/UNetDA/test/only_cnn_one_process.py
+++ code/x86/UNetDA/test/only_cnn_one_process.py
@@ -3,7 +3,6 @@ import numpy as np
 import sys
 import time
 import PostProcessor
-import TrainData 
 import Model
 import config as cf
 import sys
@@ -13,10 +12,11 @@ import netCDF4 as nc
 import scipy.ndimage
 from sys import argv
 saved_model_path_intg = {}
-saved_model_path_intg['phs'] = '/home/xuewei/ddn/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
-saved_model_path_intg['pt']  = '/home/xuewei/ddn/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
-saved_model_path_intg['u']   = '/home/xuewei/ddn/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
-saved_model_path_intg['v']   = '/home/xuewei/ddn/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
+
+saved_model_path_intg['phs'] = '/ddnstor/xuewei/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
+saved_model_path_intg['pt']  = '/ddnstor/xuewei/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
+saved_model_path_intg['u']   = '/ddnstor/xuewei/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
+saved_model_path_intg['v']   = '/ddnstor/xuewei/xiaoyi/NN_cpu/trainedModels/ResUNet3_0_14_200'
 cf.init()
 inter_num  = cf.get_inter_num()
 halo_lat = 6
@@ -54,7 +54,8 @@ def run(iternum = 0, last_result = None):
 	new_comm.barrier()
 	if comm_rank == group_root:
 		print(iternum, flush=True)
-		comm.Recv([data_sz, 10, MPI.INT], comm_rank - group_num, 0)
+		comm.Recv([data_sz, 10, MPI.INT], bias_rank - 1, 0)
+		print(data_sz, flush=True)
 	new_comm.Bcast([data_sz, 10, MPI.INT],root = int(group_rank/group_num))
 	new_comm.barrier()
 	inter_num = data_sz[9]
@@ -85,14 +86,23 @@ def run(iternum = 0, last_result = None):
 		if comm_rank == group_root:
 			recv_sz = data_sz[1] * data_sz[2] * data_sz[3] * data_sz[8]
 			recv_data = np.empty(recv_sz)
-			comm.Recv([recv_data, recv_sz, MPI.DOUBLE], comm_rank - 1, 0)
+			comm.Recv([recv_data, recv_sz, MPI.DOUBLE], bias_rank - 1, 0)
 			print('DNN: recv data',recv_data.shape,flush = True)
+			print('DNN: recv data',np.sum(recv_data),flush = True)
 			recv_data = recv_data.reshape(data_sz[3],data_sz[2],data_sz[1],data_sz[8])
 			
 			recv_data = recv_data.transpose(3,2,1,0)
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_gamma1_0/member0_step0.npy', recv_data[0])
+			# now: (ens, z, y, x)
+			# 无halo版本 发回给fortran
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_debug/' + str(iternum) + '_receive.npy', recv_data)
 			tmp_recv = recv_data
 		else:
 			recv_data = np.empty([data_sz[8],data_sz[1],data_sz[2]+halo_lat*2,data_sz[3]+halo_lon*2])
+		
+		# new_comm.barrier()
+		# new_comm.Bcast(recv_data,0)
+		# new_comm.barrier()
 
 	# ready to predict
 	if iternum == 0:
@@ -109,7 +119,7 @@ def run(iternum = 0, last_result = None):
 			total_size = recv_sz
 			result_data = result_data.reshape(total_size)
 
-			comm.Send([result_data, total_size, MPI.DOUBLE], comm_rank - 1, 0)
+			comm.Send([result_data, total_size, MPI.DOUBLE], bias_rank - 1, 0)
 			
 		del result_data
 		del recv_data
@@ -117,13 +127,16 @@ def run(iternum = 0, last_result = None):
 		if comm_rank == group_root:
 			recv_sz =  data_sz[1] * data_sz[2] * data_sz[3] * data_sz[8]
 			result_data = np.empty((data_sz[8] * data_sz[2] * data_sz[3] * new_comm.Get_size()))
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_debug/' + str(iternum) + '_ensemble_brfore_cnn.npy', ensemble)
 		else:
 			result_data = None
 		new_comm.barrier()
 		if comm_rank == group_root:
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_gamma1_0/member0_step' + str(iternum) + '_input.npy', ensemble[0])
 			print('begin predict', flush=True)
 		proxy_intgrate.data = ensemble
 		result = proxy_intgrate.predict()
+		print(result.shape, flush=True)
 		result = result.reshape(data_sz[8] *  data_sz[2] * data_sz[3]) 
 		
 		new_comm.barrier()
@@ -132,11 +145,13 @@ def run(iternum = 0, last_result = None):
 		if comm_rank == group_root:
 			result_data = result_data.reshape(new_comm.Get_size(),data_sz[8],data_sz[2],data_sz[3])
 			result_data = result_data.transpose(1,0,2,3)
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_gamma1_0/member0_step' + str(iternum) + '.npy',result_data[0])
 			result_data = result_data.transpose(3,2,1,0)
 			total_size = recv_sz
+			# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_debug/' + str(iternum) + '_result.npy', result_data)
 			result_data = result_data.reshape(total_size)
 			
-			comm.Send([result_data, total_size, MPI.DOUBLE], comm_rank - 1, 0)
+			comm.Send([result_data, total_size, MPI.DOUBLE], bias_rank - 1, 0)
 			
 		del result_data
 
@@ -144,13 +159,15 @@ def run(iternum = 0, last_result = None):
 
 	# receive ensemble data
 	if comm_rank == group_root:
-		recv_sz = data_sz[1] * data_sz[2] * data_sz[3] * total_num
+		recv_sz = data_sz[1] * data_sz[2] * data_sz[3] * data_sz[8] 
 		recv_data = np.empty(recv_sz)
-		comm.Recv([recv_data, recv_sz, MPI.DOUBLE], comm_rank - 1, 0)
+		comm.Recv([recv_data, recv_sz, MPI.DOUBLE], bias_rank - 1, 0)
 		
 		print('DNN: recv ensemble data for next round',recv_data.shape,flush = True)
-		recv_data = recv_data.reshape(data_sz[3],data_sz[2],data_sz[1],total_num)
+		recv_data = recv_data.reshape(data_sz[3],data_sz[2],data_sz[1],data_sz[8])
 		recv_data = recv_data.transpose(3,2,1,0)
+		# tmp_recv = recv_data
+		# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_gamma1_0/' + str(iternum) + '_ensemble_before_halo.npy', recv_data)
 		data_sz_lr = recv_data.shape[0]*halo_lon*recv_data.shape[1]*recv_data.shape[2]
 
 		item_send_left =  recv_data[:,:,:,: halo_lon]
@@ -173,8 +190,9 @@ def run(iternum = 0, last_result = None):
 
 		recv_data = np.append(item_recv_up,recv_data,axis =2)
 		recv_data = np.append(recv_data,item_recv_down,axis =2)
+		# np.save('/home/xuewei/ddn/jiaqilong/dida-v4/test/exp_gamma1_0/' + str(iternum) + '_ensemble_after_halo.npy', recv_data)
 	else:
-		recv_data = np.empty([total_num , data_sz[1], data_sz[2] + halo_lat * 2, data_sz[3] + halo_lon * 2])
+		recv_data = np.empty([data_sz[8] , data_sz[1], data_sz[2] + halo_lat * 2, data_sz[3] + halo_lon * 2])
 	new_comm.barrier()
 	new_comm.Bcast(recv_data, 0)
 	new_comm.barrier()
